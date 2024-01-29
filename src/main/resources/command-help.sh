76d9e345fe29ac93c4e395c085657feebcb22a66
royalihasan <royalihasan0987@gmail.com>
2024-01-29 16:07:44 +0500

    Docs + Configs + Libs => [added]

--- src/main/resources/command-help.sh
+++ src/main/resources/command-help.sh
@@ -1,10 +1,2 @@
-# Open Oracle bash
-sudo docker exec -it oracle-db bash
-# Run as sysdba
-docker exec -it oracle-db sqlplus / as sysdba
-# Run as system
-docker exec -it oracle-db sqlplus system/oracle
-# Run as user
-docker exec -it oracle-db sqlplus user/password
-# Run as user
-docker exec -it oracle-db sqlplus user/password@localhost:1521/ORCLCDB.localdomain
+#Login in Oracle
+sqlplus debezium/dbz@//localhost:1521/orclpdb1
